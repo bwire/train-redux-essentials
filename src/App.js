3705ff1626c802ada3c386255a3eeb1c34367355
barbedwire <e.barbedwire@gmail.com>
2023-08-16 08:32:26 +0200

    v1

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+
+import { Navbar } from './app/Navbar'
+import { PostsList, AddPostForm, SinglePostPage } from './features/posts'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <React.Fragment>
+                <AddPostForm />
+                <PostsList />
+              </React.Fragment>
+            )}
+          />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/features/posts/index.js
@@ -0,0 +1,5 @@
+import { AddPostForm } from './AddPostForm'
+import { PostsList } from './PostsList'
+import { SinglePostPage } from './SinglePostPage'
+
+export { AddPostForm, PostsList, SinglePostPage }
